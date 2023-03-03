5bbc8ef01acf149d3b3e4a7366213132712bab93
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-03-03 23:27:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const date = require(__dirname + "/date.js");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine","ejs")
+
+let items = ["Bread", "Milk", "Dog Food"];
+let workItems = [];
+
+app.get("/", function(req, res) {
+  let day = date.getDate();
+
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+});
+app.post("/", function(req, res) {
+  console.log(req.body);
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
